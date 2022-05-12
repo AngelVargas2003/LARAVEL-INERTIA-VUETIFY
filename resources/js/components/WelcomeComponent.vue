46b3876a450f55c091325d1af6c889b1de39a54b
AngelVargas <av422799@gmail.com>
2022-05-12 14:01:28 -0500

    Primer commit

---
+++ resources/js/components/WelcomeComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <v-row>
+            <refugios-vue></refugios-vue>
+        </v-row>
+    </div>
+</template>
+
+<script>
+import RefugiosVue from "./tables/datatable/Refugios.vue"
+    export default {
+        components:{
+            RefugiosVue
+        }
+    }
+</script>
